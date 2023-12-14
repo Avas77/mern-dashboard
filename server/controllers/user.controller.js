c97c1fddb97c544a9efea5dd94511f5959764257
Avas Bajracharya <insanebuzz09@gmail.com>
2023-12-14 18:52:04 +0545

    feat: added user controllers

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,11 @@
+import User from '../mongodb/models/user';
+
+const getAllUsers = async () => {};
+const createUser = async () => {};
+const getUserById = async () => {};
+
+export {
+    getAllUsers,
+    getUserById,
+    createUser
+}
---
+++ server/routes/property.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = new express.Router();
+
+router.post("/api/v1/create", () => {
+    
+})
+
+export default router;
