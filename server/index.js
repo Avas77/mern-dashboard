227f8537871dd84860f69a6c7053362e892954a7
Avas Bajracharya <insanebuzz09@gmail.com>
2023-12-14 18:11:32 +0545

    feat: connected to mongodb database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./mongodb/connect";
+import connectDB from "./mongodb/connect.js";
 
 dotenv.config();
 const app = express();
@@ -14,8 +14,11 @@ app.get("/", (req, res) => {
 
 const startServer = async () => {
   try {
-    connectDB(process.env.MONGODB_CONNECT);
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log("Sever has started"))
   } catch (error) {
     console.log(error);
   }
 };
+
+startServer();
