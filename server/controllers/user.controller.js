01581fc4c0346a4b3e3e1725382350dfbe81262f
Avas Bajracharya <insanebuzz09@gmail.com>
2023-12-15 12:11:34 +0545

    feat: create user from client

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,22 @@
 import User from '../mongodb/models/user.js';
 
 const getAllUsers = async (req, res) => {};
-const createUser = async (req, res) => {};
+const createUser = async (req, res) => {
+    const {name, email, avatar} = req.body;
+    try {
+        const userExists = await User.findOne({email})
+        if(userExists) return res.status(200).json(userExists);
+        const newUser = await User.create({
+            name,
+            email,
+            avatar
+        })
+        return res.status(200).json(newUser)
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+};
+
 const getUserById = async (req, res) => {};
 
 export {
